d3634a5478e68f577bcc36973225abb2f411461b
RZiln <radvilas0@gmail.com>
2021-01-24 14:18:59 +0200

    Added billboard function

--- module.lua
+++ module.lua
@@ -1,8 +1,20 @@
 local Chika = {}
 
+
 function Chika:Test()
     print("Testing")
     return 
+end
+
+function Chika:BillBoard(part)
+    local a = Instance.new("BillboardGui", part)
+    a.Size = UDim2.new(10,0, 10,0)
+    a.AlwaysOnTop = true    
+    local b = Instance.new("Frame",a)
+    b.Size = UDim2.new(1,0, 1,0)  
+    b.BackgroundTransparency = 0.75
+    b.BorderSizePixel = 0
+    b.BackgroundColor3 = Color3.new(255, 0, 255)
 end
 
 return Chika
---
+++ testing.lua
@@ -0,0 +1,7 @@
+local chika = loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/ESP-Module/main/module.lua"))()
+
+chika:Test()
+local box = Instance.new("BoxHandleAdornment", b)
+box.AlwaysOnTop = true
+box.Adornee = game:GetService("Workspace").BeaterOfBlack.LeftLowerArm
+box.Size = game:GetService("Workspace").BeaterOfBlack.LeftLowerArm.Size
