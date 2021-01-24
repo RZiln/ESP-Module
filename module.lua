d449cf90f8056f41862df83ad5bd594d99950e07
RZiln <radvilas0@gmail.com>
2021-01-24 14:30:29 +0200

    update

--- module.lua
+++ module.lua
@@ -1,20 +1,30 @@
 local Chika = {}
 
+local themes = {
+	Background = Color3.fromRGB(24, 24, 24), 
+	Glow = Color3.fromRGB(0, 0, 0), 
+	Accent = Color3.fromRGB(10, 10, 10), 
+	LightContrast = Color3.fromRGB(20, 20, 20), 
+	DarkContrast = Color3.fromRGB(14, 14, 14),  
+	TextColor = Color3.fromRGB(255, 255, 255)
+}
 
 function Chika:Test()
     print("Testing")
     return 
 end
 
-function Chika:BillBoard(part)
+
+
+function Chika:BillBoard(part, transparency)
     local a = Instance.new("BillboardGui", part)
     a.Size = UDim2.new(10,0, 10,0)
     a.AlwaysOnTop = true    
     local b = Instance.new("Frame",a)
     b.Size = UDim2.new(1,0, 1,0)  
-    b.BackgroundTransparency = 0.75
+    b.BackgroundTransparency = transparency
     b.BorderSizePixel = 0
-    b.BackgroundColor3 = Color3.new(255, 0, 255)
+    b.BackgroundColor3 = themes.Glow
 end
 
 return Chika
--- testing.lua
+++ testing.lua
@@ -1,4 +1,4 @@
 local chika = loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/ESP-Module/main/module.lua"))()
 
 chika:Test()
-chika:BillBoard(game:GetService("Workspace").BeaterOfBlack.LeftLowerArm)
+chika:BillBoard(game:GetService("Workspace").BeaterOfBlack.LeftLowerArm, 0.5)
