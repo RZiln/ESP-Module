f14977f3a75ad650d25b2f51d44abcfeebf8ef34
RZiln <radvilas0@gmail.com>
2021-01-24 14:20:39 +0200

    removed example

--- testing.lua
+++ testing.lua
@@ -1,7 +1,4 @@
 local chika = loadstring(game:HttpGet("https://raw.githubusercontent.com/RZiln/ESP-Module/main/module.lua"))()
 
 chika:Test()
-local box = Instance.new("BoxHandleAdornment", b)
-box.AlwaysOnTop = true
-box.Adornee = game:GetService("Workspace").BeaterOfBlack.LeftLowerArm
-box.Size = game:GetService("Workspace").BeaterOfBlack.LeftLowerArm.Size
+chika:BillBoard(game:GetService("Workspace").BeaterOfBlack.LeftLowerArm)
